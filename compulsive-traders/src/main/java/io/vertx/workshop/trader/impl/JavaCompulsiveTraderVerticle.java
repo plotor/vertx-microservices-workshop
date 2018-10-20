b128dae19a65b1a1d069892a4f2b098d2b9e2e79
zhenchao <zhenchao.wang@hotmail.com>
2018-10-20 17:41:40 +0800

    finish chapter 9

--- compulsive-traders/src/main/java/io/vertx/workshop/trader/impl/JavaCompulsiveTraderVerticle.java
+++ compulsive-traders/src/main/java/io/vertx/workshop/trader/impl/JavaCompulsiveTraderVerticle.java
@@ -8,21 +8,43 @@ import io.vertx.servicediscovery.types.EventBusService;
 import io.vertx.servicediscovery.types.MessageSource;
 import io.vertx.workshop.common.MicroServiceVerticle;
 import io.vertx.workshop.portfolio.PortfolioService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * A compulsive trader...
  */
 public class JavaCompulsiveTraderVerticle extends MicroServiceVerticle {
 
-  @Override
-  public void start(Future<Void> future) {
-    super.start();
+    private static final Logger log = LoggerFactory.getLogger(JavaCompulsiveTraderVerticle.class);
 
-    //TODO
-    //----
-    future.fail("no implemented yet...");
-    // ----
-  }
+    @Override
+    public void start(Future<Void> future) {
+        super.start();
 
+        // 1. Initialize the trader
+        String company = TraderUtils.pickACompany();
+        int numberOfShares = TraderUtils.pickANumber();
+        log.info("Initialize company[{}] and numberOfShares[{}]", company, numberOfShares);
+
+        // 2. Retrieve the 2 services we use
+        Future<MessageConsumer<JsonObject>> marketFuture = Future.future();
+        MessageSource.getConsumer(discovery, new JsonObject().put("name", "market-data"), marketFuture.completer());
+
+        Future<PortfolioService> portfolioFuture = Future.future();
+        EventBusService.getProxy(discovery, PortfolioService.class, portfolioFuture.completer());
+
+        // 3. When both have been retrieve, apply the trading logic on every new market data.
+        CompositeFuture.all(marketFuture, portfolioFuture).setHandler(event -> {
+            if (event.failed()) {
+                future.fail(event.cause());
+            } else {
+                marketFuture.result().handler(message ->
+                        TraderUtils.dumbTradingLogic(company, numberOfShares, portfolioFuture.result(), message.body()));
+            }
+        });
+
+        future.complete();
+    }
 
 }
